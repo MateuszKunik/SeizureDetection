995dc7b0e1495aef4ef783224f73676cd6314ccd
Mateusz Kunik <94957879+MateuszKunik@users.noreply.github.com>
2025-03-07 19:01:25 +0100

    Add files via upload

---
+++ nowe/data_setup.py
@@ -0,0 +1,62 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision.transforms import v2
+
+from .custom_dataset import CustomDataset
+
+
+def create_dataloaders(data: dict, parameters: dict) -> tuple:
+    train_dataloader = create_single_dataloader(
+        data,  subset_type="train", parameters=parameters)
+    
+    valid_dataloader = create_single_dataloader(
+        data, subset_type="valid", parameters=parameters)
+    
+    test_dataloader = create_single_dataloader(
+        data, subset_type="test", parameters=parameters)
+    
+    return train_dataloader, valid_dataloader, test_dataloader
+
+
+def create_single_dataloader(
+        data: dict,
+        subset_type: str,
+        parameters: dict
+    )  -> torch.utils.data.DataLoader:
+    data_subset = get_data_subset(data, subset_type)
+    # miejsce do zdefiniowania lub przekazania transformacji danych
+    transform = None 
+
+    if is_train_type(subset_type):
+        # miejsce do zdefiniowania lub przekazania augmentacji danych
+        augmentation = v2.Compose([
+            v2.RandomRotation(90),
+            v2.RandomHorizontalFlip(p=0.5),
+            v2.RandomVerticalFlip(p=0.5),
+
+            v2.RandomResizedCrop(64),
+            # v2.ColorJitter(brightness=0.2, contrast=0.2)
+            # v2.GaussianBlur(kernel_size=3)
+        ])
+        dataset = CustomDataset(data_subset, transform, augmentation)
+        shuffle = True
+
+    else:
+        dataset = CustomDataset(data_subset, transform)
+        shuffle=False
+    
+    dataloader = DataLoader(
+        dataset=dataset,
+        batch_size=parameters["batch_size"],
+        num_workers=parameters["num_workers"],
+        pin_memory=parameters["pin_memory"],
+        shuffle=shuffle)
+    
+    return dataloader
+
+
+def get_data_subset(data, subset_type):
+    return data[subset_type]
+
+def is_train_type(subset_type: str) -> bool:
+    return subset_type == "train"
---
+++ nowe/utilities.py
@@ -0,0 +1,67 @@
+import os
+import h5py
+import yaml
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+
+def load_model_input_data(data_directory_path: str, parameters: dict) -> tuple:
+    data_file_path = os.path.join(
+        data_directory_path,
+        f"{parameters['data_version']}.hdf5")
+    
+    with h5py.File(data_file_path, "r") as file:
+        images = file["X"][:]
+        labels = file["Y"][:]
+
+    if parameters["convolution_method"] == "2D_samples":
+        images = images.reshape(-1, 64, 64, 1).transpose(0, 3, 1, 2)
+        labels = np.repeat(labels, 18)
+
+    elif parameters["convolution_method"] == "2D_seq":
+        images = np.squeeze(images, axis=-1)
+
+    elif parameters["convolution_method"] == "3D_seq":
+        images = images.transpose(0, 4, 1, 2, 3)
+
+    return images, labels
+
+
+def get_timestamp():
+    return datetime.now().strftime("%Y-%m-%d_%H-%M")
+
+
+def generate_directory_path(target_path: str, prefix: str = None) -> str:
+    timestamp = get_timestamp()
+    directory_name = f"{prefix}_{timestamp}"
+    return os.path.join(target_path, directory_name)
+
+
+def ensure_directory_exists(target_path: str) -> str:
+    os.makedirs(target_path, exist_ok=True)
+
+
+def save_dataframe_to_csv(
+        target_path: str,
+        dataframe: pd.DataFrame,
+        file_name: str
+) -> None:
+    dataframe.to_csv(os.path.join(target_path, f"{file_name}.csv"), index=False)
+
+
+def log_dataframe_saved(file_name: str) -> None:
+    print(f"DataFrame '{file_name}.csv' has been successfully saved.")
+
+
+def save_config_params(target_path: str, parameters: dict, file_name: str) -> None:
+    with open(os.path.join(target_path, file_name), "w") as file:
+        yaml.dump(parameters, file, default_flow_style=False)
+
+
+def log_params_saved(file_name: str) -> None:
+    print(f"Configuration file '{file_name}' has been successfully saved.")
+
+
+def log_saved_file_path(target_path: str) -> None:
+    print(f"Path to saved file(s): {target_path}")
